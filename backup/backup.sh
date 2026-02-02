606c17bcdd914484bd7ade9d6867d0df775ac2b0
albercl <alberto.cuadradol@outlook.es>
2026-02-02 09:41:37 +0100

    backup/feat: add database backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -14,29 +14,36 @@ sleep 10
 
 # Generate tar file
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_FILE="/backups/backup_$TIMESTAMP.tar.gz"
+SERVER_BACKUP_FILE="/backups/server_$TIMESTAMP.tar.gz"
+DB_BACKUP_FILE="/backups/db_$TIMESTAMP.sql"
+DB_BACKUP_FILE_COMPRESSED="/backups/db_$TIMESTAMP.tar.gz"
 
-echo "Creating backup..."
+echo "Creating server backup..."
 TOTAL_SIZE=$(du -sb /data | awk '{print $1}')
-tar -c -C /data . | pv -f -s "$TOTAL_SIZE" -pterb | pigz > "$BACKUP_FILE" 2>&1
-
+tar -c -C /data . | pv -f -s "$TOTAL_SIZE" -pterb | pigz > "$SERVER_BACKUP_FILE" 2>&1
 # Set autosave back to true
 rcon-cli -a "$RCON_HOST" -p "$RCON_PASSWORD" "save-on"
 
-echo "Backup created at $BACKUP_FILE"
+echo "Server backup created at $SERVER_BACKUP_FILE"
+
+# Backup database
+docker exec -t cobblemon-conquest-database pg_dump -U admin cobblemon_conquest > $DB_BACKUP_FILE
+tar -czf "$DB_BACKUP_FILE_COMPRESSED" "$DB_BACKUP_FILE"
+rm -f "$DB_BACKUP_FILE"
+echo "Database backup created at $DB_BACKUP_FILE_COMPRESSED"
 
 # back up all files in /backup to remote via scp
-echo "Starting backup upload to ${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
+echo "Starting backups upload to ${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
 ssh-keyscan nas.albercl.dev >> /root/.ssh/known_hosts
 
-# ls /backups and store last modified file
-last_backup=$(ls -t /backups | head -n 1)
+echo "Uploading server backup ${SERVER_BACKUP_FILE} to ${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
+rsync -av --info=progress2 -e "ssh -i /root/.ssh/id" "$SERVER_BACKUP_FILE" "${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
 
-echo "Uploading backup ${last_backup} to ${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
-rsync -av --info=progress2 -e "ssh -i /root/.ssh/id" /backups/${last_backup} "${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
+echo "Uploading database backup db_${TIMESTAMP}.tar.gz to ${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
+rsync -av --info=progress2 -e "ssh -i /root/.ssh/id" "$DB_BACKUP_FILE_COMPRESSED" "${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
 
 # Remove all files in /backups
 echo "Cleaning up local backups"
 rm -f /backups/*
 
-echo "Backup upload complete."
+echo "Backups upload complete."
