c07a6750dae6442ea33cd634ff1dd66f5788efcd
albercl <alberto.cuadradol@outlook.es>
2026-02-04 22:22:44 +0100

    feat: implemented CQRS and Event Driven Design

--- discord/conquest-bot/src/RootModule.ts
+++ discord/conquest-bot/src/RootModule.ts
@@ -1,18 +1,21 @@
 import { Module } from "@nestjs/common";
-import { MemberModule } from "./member/MemberModule";
-import { PrismaModule } from "./prisma/PrismaModule";
+import { MemberModule } from "./members/MemberModule";
 import { EventEmitterModule } from "@nestjs/event-emitter";
 import { NecordModule } from "necord";
 import { IntentsBitField } from "discord.js";
-import { DiscordBotUpdates } from "./DiscordBotUpdates";
 import { ScheduleModule } from "@nestjs/schedule";
+import { CqrsModule } from "@nestjs/cqrs";
+import { PrismaModule } from "./common/prisma/PrismaModule";
+import { ExceptionsModule } from "./common/exceptions/ExceptionsModule";
+import { DiscordBotUpdates } from "./common/DiscordBotUpdates";
 
 @Module({
     imports: [
         PrismaModule.forRoot(),
-        MemberModule,
         EventEmitterModule.forRoot(),
+        ExceptionsModule,
         ScheduleModule.forRoot(),
+        CqrsModule.forRoot(),
         NecordModule.forRoot({
             token: process.env.DISCORD_BOT_TOKEN || '',
             intents: [
@@ -22,7 +25,8 @@ import { ScheduleModule } from "@nestjs/schedule";
             ],
             development: process.env.DISCORD_DEVELOPMENT_GUILD_ID ? [process.env.DISCORD_DEVELOPMENT_GUILD_ID] : false,
             skipRegistration: false,
-        })
+        }),
+        MemberModule,
     ],
     controllers: [],
     providers: [DiscordBotUpdates],
--- discord/conquest-bot/src/member/application/ports/MemberRepository.ts
+++
@@ -1,9 +0,0 @@
-import Member from "src/member/domain/models/Member";
-
-export const MemberRepositoryToken = Symbol('MemberRepository');
-export interface MemberRepository {
-
-    save(member: Member): Promise<void>;
-    findByMinecraftUuid(minecraftUuid: string): Promise<Member | null>;
-    findByDiscordId(discordId: string): Promise<Member | null>;
-}
