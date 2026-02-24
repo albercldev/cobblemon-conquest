576f923b16a30d150ede060a0bd35552d6f428f9
albercl <alberto.cuadradol@outlook.es>
2026-02-24 19:02:35 +0100

    deploy/feat: migrate database to k8s and remove old container

--- backup/backup.sh
+++ backup/backup.sh
@@ -15,8 +15,6 @@ sleep 10
 # Generate tar file
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 SERVER_BACKUP_FILE="/backups/server_$TIMESTAMP.tar.gz"
-DB_BACKUP_FILE="/backups/db_$TIMESTAMP.sql"
-DB_BACKUP_FILE_COMPRESSED="/backups/db_$TIMESTAMP.tar.gz"
 
 echo "Creating server backup..."
 TOTAL_SIZE=$(du -sb /data | awk '{print $1}')
@@ -26,21 +24,12 @@ rcon-cli -a "$RCON_HOST" -p "$RCON_PASSWORD" "save-on"
 
 echo "Server backup created at $SERVER_BACKUP_FILE"
 
-# Backup database
-docker exec -t cobblemon-conquest-database pg_dump -U admin cobblemon_conquest > $DB_BACKUP_FILE
-tar -czf "$DB_BACKUP_FILE_COMPRESSED" "$DB_BACKUP_FILE"
-rm -f "$DB_BACKUP_FILE"
-echo "Database backup created at $DB_BACKUP_FILE_COMPRESSED"
-
 # back up all files in /backup to remote via scp
 echo "Starting backups upload to ${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
 ssh-keyscan nas.albercl.dev >> /root/.ssh/known_hosts
 
 echo "Uploading server backup ${SERVER_BACKUP_FILE} to ${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
 rsync -av --info=progress2 -e "ssh -i /root/.ssh/id" "$SERVER_BACKUP_FILE" "${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
-
-echo "Uploading database backup db_${TIMESTAMP}.tar.gz to ${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
-rsync -av --info=progress2 -e "ssh -i /root/.ssh/id" "$DB_BACKUP_FILE_COMPRESSED" "${SSH_USER}@${SSH_HOST}:${SSH_PATH}"
 
 # Remove all files in /backups
 echo "Cleaning up local backups"
